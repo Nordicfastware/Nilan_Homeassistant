ca8cd6e7a82797e028aaa9276f02942d90757d13
Nordicfastware <58825251+Nordicfastware@users.noreply.github.com>
2020-03-08 20:27:29 +0100

    Create nilan_code.h

---
+++ nilan_code/nilan_code.h
@@ -0,0 +1,4 @@
+// Serial port definition in case of use of software serial
+#define SERIAL       SERIAL_HARDWARE // SERIAL_SOFTWARE or SERIAL_HARDWARE
+#define SERIAL_SOFTWARE_RX D2 // only needed if SERIAL is SERIAL_SOFTWARE
+#define SERIAL_SOFTWARE_TX D1 // only needed if SERIAL is SERIAL_SOFTWAR
